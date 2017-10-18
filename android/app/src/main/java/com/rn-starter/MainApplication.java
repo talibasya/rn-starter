52260c2aaced4257b72ad0007fdbd79a4264d74b
hstemplewski <huberts@reactpoland.com>
2017-10-18 12:51:54 +0200

    rename

--- android/app/src/main/java/com/rn-starter/MainApplication.java
+++ android/app/src/main/java/com/rn-starter/MainApplication.java
@@ -1,4 +1,4 @@
-package com.smarterinvestment;
+package com.rn-starter;
 
 import android.app.Application;
 
--- ios/rn-starterTests/rn-starterTests.m
+++ ios/rn-starterTests/rn-starterTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface smarterInvestmentTests : XCTestCase
+@interface rn-starterTests : XCTestCase
 
 @end
 
-@implementation smarterInvestmentTests
+@implementation rn-starterTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
